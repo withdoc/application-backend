ac701398f39366d9c4f93503f6bce782e2950902
sirldev <sirl0@naver.com>
2022-10-12 21:14:31 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+'use strict';
+// --express config
+var createError = require('http-errors');
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var path = require('path');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var indexRouter = require('./routes/index');
+const userRouter = require('./routes/user/users')
+
+// routers list
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	next(createError(404));
+});
+
+
+// error handler
+app.use(function(err, req, res, next) {
+	// set locals, only providing error in development
+	
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
+module.exports = app;
