808e720cf61714856062a5141f6cb2895c72bb2f
JunHyeok Ha <sirl0@naver.com>
2022-11-28 20:00:05 +0900

    feat: Document API 구현 (#18)
    * feat : create document
    
    * fix : email is stored using sha256 method
    
    * fix : remove hash func used to eamil
    
    * feat : apply getDocuments
    
    * fix : 코드 정리
    
    * feat : delete document
    
    * fix : upload document bug fix
    
    * feat : getSpecificDoc apply
    
    Co-authored-by: ChanHHOO <hpyho33@kookmin.ac.kr>

--- routes/user/users.js
+++ routes/user/users.js
@@ -10,45 +10,26 @@ const authJwt = require('../../midlewares/authJwt');
 const fabric = new FabricConfig();
 fabric.setConfig();
 
-router.get('/', async function(req, res, next) {
-  
-  
-  let result = await fabric.contract.evaluateTransaction('GetAllAssets');
-  await console.log(fabric.prettyJSONString(result));
-  res.send(200);
-});
-
 router.post('/signup', async function(req, res, next){
   const { email, password, name, address, sex, nation, birthday } = req.body;
-  await fabric.registerNewUser(sha256(email)).then(async ()=>{
-    await fabric.contract.submitTransaction('CreateUser', "", sha256(email), sha256(password), name, address, sex, nation, birthday);
+  await fabric.registerNewUser(email).then(async ()=>{
+    await fabric.contract.submitTransaction('CreateUser', "", email, sha256(password), name, address, sex, nation, birthday);
   }).catch(err => { throw new Error(`The asset ${email} already exist`); });
-  // 
-  
-  /* 
-    save data in couchdb
-  */
   res.send(200);
 })
 
 router.post('/signin', async function(req, res, next){
   const { email, password } = req.body;
-  const emailHashedValue = sha256(email);
   const passwordHashedValue = sha256(password)
-  const exist = fabric.contract.evaluateTransaction("UserExists", emailHashedValue, passwordHashedValue)
-  console.log(exist)
+  const exist = fabric.contract.evaluateTransaction("UserExists", email, passwordHashedValue)
   if (exist) { // id, pw가 맞다면..
     const user = {
-      id:emailHashedValue,
+      id:email,
       role:"user"
     }
-    // access token과 refresh token을 발급합니다.
+    // access token을 발급합니다.
+    
     const accessToken = await jwt.sign(user);
-    // const refreshToken = await jwt.refresh();
-
-    // 발급한 refresh token을 redis에 key를 user의 id로 하여 저장합니다.
-    // await redisClient.set(user.id, refreshToken);
-
     res.status(200).send({ // client에게 토큰 모두를 반환합니다.
       ok: true,
       data: {
@@ -63,18 +44,18 @@ router.post('/signin', async function(req, res, next){
   }
 })
 
-router.put('/modify', authJwt, function(req, res, next){
-  const { email, password } = req.body;
-  if(true){
-    
-  }
-  res.send(200);
+router.put('/modify', authJwt, async function(req, res, next){
+  const { email, password, newPassword} = req.body;
+  const passwordHashedValue = sha256(password);
+  const newPasswordHashedValue = sha256(newPassword);
+  await fabric.contract.submitTransaction("ModifyPassword", email, passwordHashedValue, newPasswordHashedValue)
+    .then(()=>{res.send(200)})
+    .catch(err => {res.send(401)})
 })
 
 router.post('/delete', authJwt, async (req,res,next) => {
-  console.log(1111)
   const { email, password } = req.body;
-  const emailHashedValue = sha256(email);
+  const emailHashedValue = email;
   const passwordHashedValue = sha256(password)
   await fabric.contract.submitTransaction("DeleteUser", emailHashedValue, passwordHashedValue)
     .then(()=>{res.send(200)})
